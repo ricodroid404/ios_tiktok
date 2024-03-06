48c440d9c41c3e56d3071a9ca17dd3f3f151edef
ricodroid404 <ricodroid2024@gmail.com>
2024-03-06 20:24:40 +0900

    S3上の動画をスクロール表示できる。

--- ios_TikTok_scroll/MainViewController.swift
+++ ios_TikTok_scroll/MainViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVKit
 
 class MainViewController: UIViewController {
     
@@ -13,50 +14,63 @@ class MainViewController: UIViewController {
     @IBOutlet weak var scrollView: UIScrollView!
     
     override func viewDidLoad() {
-            super.viewDidLoad()
-            
-            // 一つのUIViewに要素を追加
-            let containerView = UIView()
-            containerView.translatesAutoresizingMaskIntoConstraints = false
-            scrollView.addSubview(containerView)
-            scrollView.isPagingEnabled = true
-            
-            // containerViewの制約設定
-            NSLayoutConstraint.activate([
-                containerView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
-                containerView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
-                containerView.topAnchor.constraint(equalTo: scrollView.topAnchor),
-                containerView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
-                containerView.widthAnchor.constraint(equalTo: scrollView.widthAnchor)
-            ])
-            
-            // 各ビューの色を定義した配列
-            let colors: [UIColor] = [.systemRed, .systemYellow, .systemOrange, .systemBlue, .systemGreen]
-            
-            // 各ビューを作成して配置する
-            var previousView: UIView?
-            for color in colors {
-                let view = UIView()
-                view.backgroundColor = color
-                containerView.addSubview(view)
-                view.translatesAutoresizingMaskIntoConstraints = false
-                
-                NSLayoutConstraint.activate([
-                    view.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
-                    view.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
-                    view.topAnchor.constraint(equalTo: previousView?.bottomAnchor ?? containerView.topAnchor),
-                    view.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
-                    view.heightAnchor.constraint(equalTo: scrollView.heightAnchor)
-                ])
-                
-                previousView = view
+        super.viewDidLoad()
+        
+        // 一つのUIViewに要素を追加
+        let containerView = UIView()
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.addSubview(containerView)
+        scrollView.isPagingEnabled = true
+        
+        // containerViewの制約設定
+        NSLayoutConstraint.activate([
+            containerView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            containerView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            containerView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            containerView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            containerView.widthAnchor.constraint(equalTo: scrollView.widthAnchor)
+        ])
+        
+        // 各動画のURLを定義した配列
+        let videoURLs = [
+            "https://test-pvg-video-contents-bucket.s3.ap-northeast-1.amazonaws.com/flower.mp4",
+            "https://test-pvg-video-contents-bucket.s3.ap-northeast-1.amazonaws.com/test_30mb.mp4",
+            "https://test-pvg-video-contents-bucket.s3.ap-northeast-1.amazonaws.com/Video+MP4_Moon+-+testfile.org.mp4",
+            "https://test-pvg-video-contents-bucket.s3.ap-northeast-1.amazonaws.com/pexels-cristian-rossa-20208157+(Original).mp4",
+            "https://test-pvg-video-contents-bucket.s3.ap-northeast-1.amazonaws.com/file_example_MP4_1920_18MG.mp4"
+        ]
+        
+        // 各動画を表示するAVPlayerViewControllerを作成して配置する
+        var previousView: UIView?
+        for videoURLString in videoURLs {
+            guard let videoURL = URL(string: videoURLString) else {
+                continue
             }
             
-            // 最後のビューのbottomAnchorをcontainerViewのbottomAnchorに合わせる
+            let player = AVPlayer(url: videoURL)
+            let playerViewController = AVPlayerViewController()
+            playerViewController.player = player
+            addChild(playerViewController)
+            containerView.addSubview(playerViewController.view)
+            playerViewController.view.translatesAutoresizingMaskIntoConstraints = false
+            
             NSLayoutConstraint.activate([
-                previousView!.bottomAnchor.constraint(equalTo: containerView.bottomAnchor)
+                playerViewController.view.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+                playerViewController.view.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+                playerViewController.view.topAnchor.constraint(equalTo: previousView?.bottomAnchor ?? containerView.topAnchor),
+                playerViewController.view.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+                playerViewController.view.heightAnchor.constraint(equalTo: scrollView.heightAnchor)
             ])
+            
+            playerViewController.didMove(toParent: self)
+            player.play()
+            
+            previousView = playerViewController.view
         }
-    
+        
+        // 最後の動画プレーヤーのbottomAnchorをcontainerViewのbottomAnchorに合わせる
+        NSLayoutConstraint.activate([
+            previousView!.bottomAnchor.constraint(equalTo: containerView.bottomAnchor)
+        ])
+    }
 }
-
